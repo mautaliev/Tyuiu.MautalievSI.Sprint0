0ba161dd767aa470b0970f7dcf84aa2c0e0332db
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-03-24 22:53:23 +0500

    Task2

---
+++ Tyuiu.MautalievSI.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MautalievSI.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MautalievSI.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Саидамир";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Саидамир", res);
+        }
+    }
+}
---
+++ Tyuiu.MautalievSI.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MautalievSI.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MautalievSI.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f65d199f-ef2b-4817-b7cd-5eb51e2653e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
